a3e1ae9a3f4354b23c078073b1cc7997606b4782
Akash Patil <48435105+akashpatil1996@users.noreply.github.com>
2023-06-29 16:38:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API)
 if 'buffer_memory' not in st.session_state:
     st.session_state.buffer_memory = ConversationBufferWindowMemory(k=2, return_messages=True)
 
-system_msg_template = SystemMessagePromptTemplate.from_template(template="""You are Sadhguru, a yogi, mystic and a spiritual guru, answer the question with only the context provided and as truthful as possible, and if the answer is not contained within the text below, say 'I don't know'. Context: """)
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""You are Sadhguru, a yogi, mystic and a spiritual guru, answer the question with only the context provided and as truthful as possible, and if the answer is not contained within the text below, say 'I don't know'. Do not provide any prefix, just output the answer. Context: """)
 
 human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
